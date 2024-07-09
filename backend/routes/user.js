f515db3494d7488d8d38efde51599aefd92dbb9f
aayush0325 <ayush25khanna@gmail.com>
2024-07-09 11:08:50 +0530

    user routes completed

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -20,11 +20,71 @@ const signinSchema = zod.object({
   lastname: zod.string().min(3).max(50),
 })
 
-router.post('/signup',(req,res) => {
+router.post('/signup',async (req,res) => {
+  const success = signupSchema.safeParse(req.body).success;
+  if(!success){
+    return res.status(411).json({
+      msg:"Invalid inputs",
+    })
+  }
+
+  const existingUser = await User.findOne({
+    username:req.body.username,
+  });
+
+  if(existingUser){
+    return res.status(411).json({
+      msg:"Username Already Exists"
+    })
+  }
+
+  const user = await User.create({
+    username:req.body.username,
+    password:req.body.password,
+    firstname:req.body.firstname,
+    lastname:req.body.lastname,
+  })
+
+  const userID = user._id;
+
+  const token = jwt.sign({
+    userID,
+  },JWT_SECRET);
+
+  return res.status(200).json({
+    msg:"User Created Successfully",
+    token:token,
+  })
 
 })
 
+router.post('/signin',async (req,res) => {
+    const success = signinSchema.safeParse(req.body).success;
+  if(!success){
+    return res.status(411).json({
+      msg: "Invalid inputs"
+    })
+  }
 
+  const existingUser = User.findOne({
+    username:req.body.username,
+  })
+
+  if(!existingUser){
+    return res.status(411).json({
+      msg:"User not found"
+    })
+  }
+
+  const token = jwt.sign({
+    userID: existingUser._id,
+  },JWT_SECRET);
+
+  return res.status(200).json({
+    token,
+  })
+
+})
 module.exports = router;
 
 
