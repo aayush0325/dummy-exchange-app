b72d04942f0be587c9a3476c903c260eeb761ba5
aayush0325 <ayush25khanna@gmail.com>
2024-07-11 14:31:42 +0530

    rewriting frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Signup } from './components/signup'
 import { Signin } from './components/signin'
-import { Dashboard } from './components/dashboard'
-import { SendMoney } from './components/sendmoney'
+
 
 
 function App() {
@@ -15,8 +14,6 @@ function App() {
         <Routes>
           <Route path='/signup' element={<Signup/>}/>
           <Route path="/signin" element={<Signin />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} />
         </Routes>
       </BrowserRouter>
    </>
